4137fcd5b3c8b7aef98005cd36c6ef5b0c973d7f
Juan <juangomezvillagithub@gmail.com>
2026-03-06 19:08:05 +0100

    Campos para localización

--- Database/1. Tables.sql
+++ Database/1. Tables.sql
@@ -11,11 +11,24 @@ CREATE OR REPLACE TABLE users (
     gender ENUM('male', 'female', 'unknown'),               -- Gender (if null, it is considered unknown)
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Creation date
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Modification date
-    delete_at TIMESTAMP NULL DEFAULT NULL
+    delete_at TIMESTAMP NULL DEFAULT NULL,                  -- Delete date
+    country_code CHAR(2),                                   -- ISO 3166 (ES, US, FR...)
+    city VARCHAR(200),                                      -- City
+    postal_code VARCHAR(20),                                -- Postal code
+    latitude DECIMAL(9,6),                                  -- Latitude
+    longitude DECIMAL(9,6)                                  -- Longitude
 ) ENGINE=InnoDB;
 
--- SELECT id, username, name, surname, email, user_type, enabled, gender, create_at, update_at, delete_at FROM users;
+-- Values
 INSERT INTO users (username, name, surname, password, email, user_type, enabled, gender, create_at, update_at) VALUES
     ('user1', 'Nombre1', 'Apellido1', SHA2('1234', 512), 'correo1@email.com', 'admin',       1, 'male',     NOW(), NOW()),
     ('user2', 'Nombre2', 'Apellido1', SHA2('1234', 512), 'correo2@email.com', 'sponsor',     1, 'female',   NOW(), NOW()),
     ('user3', 'Nombre3', 'Apellido1', SHA2('1234', 512), 'correo3@email.com', 'organizer',   1, 'unknown',  NOW(), NOW());
+
+-- Query
+SELECT id,
+    username,
+    name, surname,
+    email, user_type, enabled, gender,
+    create_at, update_at, delete_at
+FROM users;
